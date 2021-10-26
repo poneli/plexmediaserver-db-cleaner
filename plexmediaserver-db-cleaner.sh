ce822e4455b6bc9caa78cb684d683ae9cf39200b
poneli <elisson.pontus@gmail.com>
2021-10-26 14:49:06 +0200

    First version

---
+++ plexmediaserver-db-cleaner.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+#### Description: Upgrades plexmediaserver for debian/ubuntu based distros
+####
+#### Written by: poneli on 2021 October 3
+#### Published on: https://github.com/poneli/
+#### =====================================================================
+#### <VARIABLES>
+plexsqlite="/usr/lib/plexmediaserver/Plex SQLite"
+plexdir="/var/lib/plexmediaserver/Library/Application Support/Plex Media Server/Plug-in Support/Databases" # No trailing slash
+plexdb="com.plexapp.plugins.library.db"
+plexdb_bak="com.plexapp.plugins.library.db.bak"
+plexdb_size=$(du -km $plexdir/$plexdb)
+#### </VARIABLES>
+if [[ $EUID -gt 0 ]]; then
+	printf "Run with sudo... \n"
+	exit
+fi
+	printf "Stopping plexmediaserver ... \n"
+	systemctl stop plexmediaserver
+	printf "Backing up database to file %s ... \n" "$plexdb_bak"
+	cp $plexdir/$plexdb $plexdir/$plexdb_bak
+	printf "Plex dB is %sMB before cleanup/fix ... \n" "$plexdb_size"
+	printf "Dumping dB to file dump.sql ... \n"
+	$plexsqlite $plexdir/$plexdb ".output $plexdir/dump.sql" ".dump"
+	printf "Deleting old Plex dB ... \n"
+	rm $plexdir/$plexdb
+	printf "Importing new Plex dB from dump.sql ... \n"
+	$plexsqlite $plexdir/$plexdb ".read $plexdir/dump.sql"
+	printf "Changing owner of Plex dB ... \n"
+	chown plex:plex $plexdir/$plexdb
+	printf "Plex dB is %sMB after cleanup/fix ... \n" "$plexdb_size"
+	printf "Cleanup files ... \n"
+	rm dump.sql com.plexapp.plugins.library.db-shm com.plexapp.plugins.library.db-wal
+	printf "Starting plexmediaserver ... \n"
+	printf "Done ... \n"
+exit 0
