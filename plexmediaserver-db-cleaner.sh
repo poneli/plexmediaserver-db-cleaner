0c6fa86a69b3e1de87793b4ee7c1ea4fdb518971
poneli <elisson.pontus@gmail.com>
2021-10-26 17:10:55 +0200

    Fix header

--- plexmediaserver-db-cleaner.sh
+++ plexmediaserver-db-cleaner.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
-#### Description: Upgrades plexmediaserver for debian/ubuntu based distros
+#### Description: plexmediaserver database cleaner
 ####
-#### Written by: poneli on 2021 October 3
+#### Written by: poneli on 2021 October 26
 #### Published on: https://github.com/poneli/
 #### =====================================================================
 #### <VARIABLES>
